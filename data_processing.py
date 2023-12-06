474c543d349b5acd0d43f2b6787e3ffaabe071b2
EnzoMGitHub <vincenzomonterosso9@gmail.com>
2023-12-06 13:26:02 -0500

    Data_pro wip, and button note added to gui_plot.py

--- data_processing.py
+++ data_processing.py
@@ -3,7 +3,54 @@ from dotenv import load_dotenv
 
 load_dotenv() 
 
+# Functions
+  
+def occurances(word,lis):
+    count = 0
+    for i in lis:
+        if i == word:
+            count = count + 1
+    return count
+
+def remove_word(word,lis): # Work in progress
+    ret = lis
+    while word in ret:
+        ret.remove(word)
+    ret.append(word)
+    return ret
+
+# Data Processing
 file = os.getenv("file")
-file = open(file,'r')
-    
-file.close()
+f = open(file,'r')
+lines = f.read()
+lines = lines.split()
+
+list_of_words = lines[:]
+list_of_occurr = list()
+
+ # Work in progress
+# for i in lines:
+#     if i.isalpha():
+#         list_of_words = remove_word(i,list_of_words)
+#         list_of_occurr.append(occurances(i,lines))
+#     else:
+#         new_word = ''
+#         for j in i:
+#             if j.isalpha():
+#                 new_word = new_word + j
+#         list_of_words = remove_word(i,list_of_words)
+#         list_of_occurr.append(occurances(i,lines))
+for i in lines:
+    word = ''
+    for j in i:
+        if j.isalpha():
+            word = word + j
+    list_of_words = remove_word(i,list_of_words)
+    list_of_occurr.append(occurances(i,lines))
+
+
+print(lines)
+print('No Dupe',list_of_words)
+print(list_of_occurr)
+
+f.close()
--- gui_plot.py
+++ gui_plot.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import ttk
 import matplotlib
 
+# Notes
+# The button type that will be used for word mode and character mode is called radio button I think
 root = Tk()
 frm = ttk.Frame(padding=10)
 frm.grid()
