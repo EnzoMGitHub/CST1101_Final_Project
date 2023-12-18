3603120897a427f4533bec90e379ab0b9128c0bb
EnzoMGitHub <136654219+EnzoMGitHub@users.noreply.github.com>
2023-12-18 12:21:57 -0500

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,57 +1,81 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv() 
+# Word Frequency Calculator 
+from tkinter import *
+from tkinter import ttk
+import matplotlib.pyplot as plot
 
-# Functions
-  
-def occurances(word,lis):
+def remove_punctuation(string):
+    ret = ''
+    for i in string:
+        if i.lower() in 'abcdefghijklmnopqrstuvwxyz':
+            ret = ret + i.lower()
+    return ret
+def frequency(lis, word):
     count = 0
     for i in lis:
         if i == word:
             count = count + 1
-    return count
-
-def remove_word(word,lis): # Work in progress
-    ret = lis
-    while word in ret:
-        ret.remove(word)
-    ret.append(word)
+    return count/len(lis)
+def remove_duplicates(lis):
+    ret = list()
+    for i in lis:
+        if i.lower() in ret:
+            continue
+        else:
+            ret.append(i.lower())
     return ret
+def merge(lis1,lis2):
+    return {lis1[n]:lis2[n] for n in range(len(lis1))}
+def word_frequencies(initial_words):
+    
+    words = list()
+    no_dupe = list()
+    for i in initial_words:
+        words.append(remove_punctuation(i))
 
-# Data Processing
-file = os.getenv("file")
-f = open(file,'r')
-lines = f.read()
-lines = lines.split()
+    no_dupe = remove_duplicates(words)
+
+    counts = list()
+    for i in no_dupe:
+        counts.append(frequency(words,i))
+    
+    return merge(no_dupe,counts)
+
+def char_frequencies(lis):
+    chars = list(''.join(initial_words).lower())
+    no_dupe_chars = remove_duplicates(chars)
+    freqs = list()
+    for i in no_dupe_chars:
+        freqs.append(frequency(chars,i))
+        
+    return merge(no_dupe_chars,freqs)
+
+    
+def plot_words():
+    plot.bar(dictionary.keys(),dictionary.values())
+    
+    # plot.xticks(rotation=70)
+    plot.show()
+
+def plot_chars():
+    plot.bar(char_dict.keys(),char_dict.values())
+    plot.show()
+
+file = 0 # Creating a file variable that will allow the while loop to work
+while file == 0: # If the file variable doesnt get updated continue the loop, this allows the user to get unlimited attempts 
+    try:
+        file = open(input('What is the file path of the text file?'),'r')
+        initial_words = file.read().split()
+    except:
+        print('This file does not exist')
+dictionary = word_frequencies(initial_words)
+char_dict = char_frequencies(initial_words)
 
 
-list_of_occurr = list()
-
- # Work in progress
-# for i in lines:
-#     if i.isalpha():
-#         list_of_words = remove_word(i,list_of_words)
-#         list_of_occurr.append(occurances(i,lines))
-#     else:
-#         new_word = ''
-#         for j in i:
-#             if j.isalpha():
-#                 new_word = new_word + j
-#         list_of_words = remove_word(i,list_of_words)
-#         list_of_occurr.append(occurances(i,lines))
-list_of_words = lines[:]
-# for i in lines:
-#     word = ''
-#     for j in i:
-#         if j.isalpha():
-#             word = word + j
-#     list_of_words = remove_word(word,list_of_words)
-#     list_of_occurr.append(occurances(i,lines))
-
-
-print(lines)
-print('No Dupe',list_of_words)
-print(list_of_occurr)
-
-f.close()
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Radiobutton(frm,text='Word Mode',command=plot_words,value=1).grid(column=0,row=1)
+ttk.Radiobutton(frm,text='Character Mode',command=plot_chars,value=2).grid(column=0,row=2)
+root.mainloop()
+file.close()
